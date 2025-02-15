4ff024ed8a1918e31e4eee6b3abeaa580080b35c
FleetingComet <hakanai.suisei@gmail.com>
2025-02-15 23:58:44 +0800

    Adjust Item name search region

--- src/locations/search.py
+++ src/locations/search.py
@@ -6,7 +6,7 @@ from area import Region
 # 1280x720p
 class SearchPattern(Enum):
     ITEM_OWNED = Region(480, 510, 100, 40)
-    ITEM_NAME = Region(50, 480, 430, 70)
+    ITEM_NAME = Region(55, 480, 430, 70)  # plus 5 for x (to avoid ')
 
     EQUIPMENT_OWNED = Region(530, 595, 90, 30)  # plus 5 for y (for ocr to detect it)
     EQUIPMENT_NAME = Region(60, 560, 420, 80)
